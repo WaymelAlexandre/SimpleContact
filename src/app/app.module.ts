9163a6dd6afaf9ebf0d4ff13d41608997d52061a
alex w <103083020@student.swin.edu.au>
2020-09-22 10:11:27 +1000

    frist compo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewcontactComponent } from './newcontact/newcontact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewcontactComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/newcontact/newcontact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newcontact',
+  templateUrl: './newcontact.component.html',
+  styleUrls: ['./newcontact.component.css']
+})
+export class NewcontactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
